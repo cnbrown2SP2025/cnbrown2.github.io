1fa8fbfd91471abb34d216c567be63af4e834852
cnbrown2SP2025 <cnbrown2@my.waketech.edu>
2025-04-10 13:35:19 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+// Stub: Fetch grade data from the database (future functionality)
+function fetchGradeData() {
+  console.log("fetchGradeData() called");
+  // Placeholder: This function will retrieve data from the backend/database
+}
+
+// Stub: Populate the gradebook table with data (future functionality)
+function populateGradebook() {
+  console.log("populateGradebook() called");
+  // Placeholder: This function will use data to populate the table
+}
+
+// Call stubs to test
+fetchGradeData();
+populateGradebook();
